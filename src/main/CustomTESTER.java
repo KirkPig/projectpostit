8f48d4bbcffb66cef31391806e20fe2ce7265692
tonnaminotheus <tonnam_t@windowslive.com>
2020-06-27 22:26:22 +0700

    QYselection Born

--- src/main/CustomTESTER.java
+++ src/main/CustomTESTER.java
@@ -1,12 +1,15 @@
 package main;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import ui.CustomerBox;
 import ui.GeneralBox;
 import ui.QYBox;
+import ui.QYselection;
 public class CustomTESTER extends Application{
 	public static void main(String[] args) {
 		launch(args);
@@ -15,15 +18,9 @@ public class CustomTESTER extends Application{
 	@Override
 	public void start(Stage arg0) throws Exception {
 		// TODO Auto-generated method stub
-		CustomerBox a = new CustomerBox(400,200);
-		GeneralBox general = new GeneralBox(100,200);
-		QYBox other = new QYBox(100,200);
-		VBox left = new VBox();
 		
-		left.getChildren().addAll(general,other);
-		HBox upper = new HBox();
-		upper.getChildren().addAll(left,a);
-		Scene testScene = new Scene(upper);
+		Scene testScene = new Scene(new QYselection());
+		arg0.setTitle("BETA TESTER");
 		arg0.setScene(testScene);
 		arg0.setResizable(false);
 		arg0.show();
