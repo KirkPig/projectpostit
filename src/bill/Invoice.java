b3440d00deb296b4cee59119b5e801d541553c02
Kirk Pig <kriklomark@outlook.com>
2020-07-02 14:13:10 +0700

    edited bill component

--- src/bill/Invoice.java
+++ src/bill/Invoice.java
@@ -9,23 +9,34 @@ public class Invoice {
 	private String id;
 	private String date;
 	private Customer customer;
+	private String poNum;
+	private String orderBy;
+	private String paymentTerm;
+	private String dateDue;
+	private String sales;
 	private ArrayList<Item> itemList;
 	private double valueBeforeTax;
 	private double valueTax;
 	private double valueAfterTax;
 
-	public Invoice(String id, String date, Customer customer, ArrayList<Item> itemList, String attn, String cr) {
+	public Invoice(String id, String date, Customer customer, ArrayList<Item> itemList, String poNum, String orderBy,
+			String paymentTerm, String dateDue, String sales) {
 		this.setCustomer(customer);
 		this.setDate(date);
 		this.setId(id);
 		this.setItemList(itemList);
+		this.setPoNum(poNum);
+		this.setOrderBy(orderBy);
+		this.setPaymentTerm(paymentTerm);
+		this.setDateDue(dateDue);
+		this.setSales(sales);
 		double total = 0.00;
 		for (Item e : itemList) {
 			total += e.getAmount();
 		}
 		this.setValueBeforeTax(total);
 		this.setValueTax(total * 7 / 100);
-		this.setValueAfterTax(getValueBeforeTax() + getValueTax()); 
+		this.setValueAfterTax(getValueBeforeTax() + getValueTax());
 	}
 
 	public String getId() {
@@ -82,5 +93,45 @@ public class Invoice {
 
 	public void setValueAfterTax(double valueAfterTax) {
 		this.valueAfterTax = valueAfterTax;
+	}
+
+	public String getPoNum() {
+		return poNum;
+	}
+
+	public void setPoNum(String poNum) {
+		this.poNum = poNum;
+	}
+
+	public String getOrderBy() {
+		return orderBy;
+	}
+
+	public void setOrderBy(String orderBy) {
+		this.orderBy = orderBy;
+	}
+
+	public String getPaymentTerm() {
+		return paymentTerm;
+	}
+
+	public void setPaymentTerm(String paymentTerm) {
+		this.paymentTerm = paymentTerm;
+	}
+
+	public String getDateDue() {
+		return dateDue;
+	}
+
+	public void setDateDue(String dateDue) {
+		this.dateDue = dateDue;
+	}
+
+	public String getSales() {
+		return sales;
+	}
+
+	public void setSales(String sales) {
+		this.sales = sales;
 	}
 }
