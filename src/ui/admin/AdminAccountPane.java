623e57df7f78b22f31777dd95ca188a900637ec4
sarasix <saralovemom@hotmail.com>
2020-07-06 22:03:27 +0700

    login+admin

--- src/ui/admin/AdminAccountPane.java
+++ src/ui/admin/AdminAccountPane.java
@@ -1,19 +1,31 @@
 package ui.admin;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import com.google.gson.Gson;
 
 import database.User;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TablePosition;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TableView.TableViewSelectionModel;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import logic.DatabaseConnection;
 
 public class AdminAccountPane extends VBox {
 	
@@ -21,10 +33,15 @@ public class AdminAccountPane extends VBox {
 	private TableView<User> accountTable = new TableView<User>();
 	private HBox accountAddBox = new HBox();
 	private TextField usernameField = new TextField();
-	private PasswordField passwordField = new PasswordField();
+	private TextField passwordField = new PasswordField();
 	private TextField nameField = new TextField();
 	private Button btnAddAccount = new Button("Add");
-	
+	private HBox accountChangeBox = new HBox();
+	private TextField usernameField2 = new TextField();
+	private TextField passwordField2 = new TextField();
+	private TextField nameField2 = new TextField();
+	private Button btnChangeAccount = new Button("Change");
+	private String usernamebf ;
 	@SuppressWarnings("unchecked")
 	public AdminAccountPane() {
 
@@ -46,6 +63,10 @@ public class AdminAccountPane extends VBox {
 		accountAddBox.setSpacing(5);
 		accountAddBox.setMinHeight(50);
 		
+		accountChangeBox.setAlignment(Pos.CENTER);
+		accountChangeBox.setSpacing(5);
+		accountChangeBox.setMinHeight(50);
+		
 		usernameField.setPromptText("Username");
 		passwordField.setPromptText("Password");
 		nameField.setPromptText("Name");
@@ -61,28 +82,125 @@ public class AdminAccountPane extends VBox {
 			}
 			
 		});
-		
+		btnChangeAccount.setMinWidth(100);
+		btnChangeAccount.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			
+			public void handle(MouseEvent event) {
+	
+				editUser();
+			}
+			
+		});
 		accountAddBox.getChildren().add(usernameField);
 		accountAddBox.getChildren().add(passwordField);
 		accountAddBox.getChildren().add(nameField);
 		accountAddBox.getChildren().add(btnAddAccount);
 		
+		accountChangeBox.getChildren().add(usernameField2);
+		accountChangeBox.getChildren().add(passwordField2);
+		accountChangeBox.getChildren().add(nameField2);
+		accountChangeBox.getChildren().add(btnChangeAccount);
+		
+		
 		getChildren().add(accountTable);
 		getChildren().add(accountAddBox);
+		getChildren().add(accountChangeBox);
 		
+		
+		
+		updateTable();
+		
+		accountTable.setRowFactory(tv -> {
+			TableRow<User> row = new TableRow<>();
+		    row.setOnMouseClicked(event -> {
+		        if (! row.isEmpty() && event.getButton()==MouseButton.PRIMARY 
+		             && event.getClickCount() == 2) {
+
+		            User clickedRow = row.getItem();
+		            usernamebf = clickedRow .getUsername();
+		            System.out.println(clickedRow .getName());
+		            usernameField2.setText(clickedRow .getUsername());
+		    		passwordField2.setText(clickedRow .getPassword());
+		    		nameField2.setText(clickedRow .getName());
+		    		
+		        }
+		    });
+		    return row ;
+		});
+
+		
+
 	}
 	
 	private void addNewAccount() {
-
+		try {
+		Connection conn = DatabaseConnection.getConnection();
 		User user = new User(usernameField.getText(), passwordField.getText(), nameField.getText());
 		String sql = "insert into account values('" + usernameField.getText() + "','" + passwordField.getText() + "','"
 				+ nameField.getText() + "','" + gson.toJson(user) + "')";
 		
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		int x = stmt.executeUpdate();
+		
+		
+		}
+		catch(Exception e1) {
+			System.out.println(e1);
+		};
+		
+		addTable();
 		accountAddBoxClear();
 
 	}
-	
+	private void updateTable() {
+		accountTable.getItems().clear();
+		try {
+			Connection conn = DatabaseConnection.getConnection();
+			Statement stmt = conn.createStatement();
+			String sql = "select * from account;";
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			
+			while(rs.next()) {
+				String username = rs.getString("username");
+				String password = rs.getString("password");
+				String name = rs.getString("name");
+				User user = new User (username,password,name);
+				accountTable.getItems().add(user);
+				
+			}
+			
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	private void addTable() {
+		accountTable.getItems().add(new User(usernameField.getText(), passwordField.getText(),nameField.getText()));
+		
+	}
+	private void editUser() {
+		try {
+			Connection conn = DatabaseConnection.getConnection();
+			User user = new User(usernameField.getText(), passwordField.getText(), nameField.getText());
+			String sql = "UPDATE account SET username = '" + usernameField2.getText() + "',password = '"+ passwordField2.getText() +"',name = '"+nameField2.getText() + "'"+"WHERE username = '"+usernamebf+"';";
+			System.out.println(sql);
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			int x = stmt.executeUpdate();
+			
+			}
+		catch(Exception e1) {
+				System.out.println(e1);
+			};
+		updateTable();
+		
+	}
 	private void accountAddBoxClear() {
+		
+		
 		
 		usernameField.clear();
 		passwordField.clear();
