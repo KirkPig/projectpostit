0737c2f901361387c962d8b0c52e42693169d40a
tonnaminotheus <tonnam_t@windowslive.com>
2020-07-07 11:37:28 +0700

    bur

--- src/ui/selection/QYSelection.java
+++ src/ui/selection/QYSelection.java
@@ -79,8 +79,8 @@ public class QYSelection extends VBox {
 		search.setPromptText("Search");
 		ComboBox<String> genre = new ComboBox<String>();
 		genre.getItems().addAll("Code", "Product", "Customer Name", "Creator", "Amount");
-
-		simpleFunc.getChildren().addAll(newButton, new Button("open/edit"), , new Button("bin"));
+		
+		simpleFunc.getChildren().addAll(newButton, new Button("open/edit"), deleteBtn, new Button("bin"));
 		simpleFunc.setSpacing(3);
 		moreFunc.getChildren().addAll(new Button("print report"), switchButton, month, year);
 		moreFunc.setSpacing(3);
