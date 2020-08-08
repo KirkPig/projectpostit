a2fb69967b9358d70dc31f9594825157172a115c
sarasix <saralovemom@hotmail.com>
2020-08-08 17:07:15 +0700

    ui+m

--- src/ui/admin/AdminUI.java
+++ src/ui/admin/AdminUI.java
@@ -1,9 +1,14 @@
 package ui.admin;
 
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import ui.selection.Login;
 
 public class AdminUI extends VBox {
 	
@@ -24,10 +29,22 @@ public class AdminUI extends VBox {
 		btnAccount.setPrefSize(200, 50);
 		tab.getChildren().add(btnAccount);
 		
-		/*
-		 * Account Tab
-		 */
-		
+		btnAccount.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent event) {
+				stage.close();
+//				Stage arg0 = new Stage();
+//				
+//				Login login = new Login(arg0);
+//				Scene testScene = new Scene(login);
+//				arg0.setTitle("yonotool");
+//				Image logo = new Image ("/res/yono_logo.png");
+//				arg0.getIcons().add(logo);
+//				arg0.setScene(testScene);
+//				arg0.setResizable(false);
+//				arg0.show();
+			}
+
+		});
 		
 		getChildren().add(tab);
 		getChildren().add(adminAccountPane);
