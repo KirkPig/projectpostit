84a9afa7bb1dd009c1359790537b1c129aa7ed70
Kirk Pig <kriklomark@gmail.com>
2021-10-14 23:12:20 +0700

    Update the DatabaseConnection

--- src/logic/DatabaseConnection.java
+++ src/logic/DatabaseConnection.java
@@ -8,8 +8,8 @@ public class DatabaseConnection {
 	public static Connection getConnection() throws Exception {
 		
 		Class.forName("com.mysql.jdbc.Driver");
-		return DriverManager.getConnection("jdbc:mysql://103.253.72.156:3306/yonotool_app?useUnicode=true&characterEncoding=utf-8", "yonotool",
-				"2aF::on0T8E6oY");
+		return DriverManager.getConnection("<database host>", "<database username>",
+				"<database password>");
 
 	}
 
